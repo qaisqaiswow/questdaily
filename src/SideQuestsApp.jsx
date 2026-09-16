acb6570096e417c99d53f0d5113958fc39d79a68
Qais Kurdieh <qaiskurdieh79@gmail.com>
2026-09-16 15:33:39 +0300

    Refactor CircleUserRound component props

--- src/SideQuestsApp.jsx
+++ src/SideQuestsApp.jsx
@@ -1172,7 +1172,7 @@ return (
 </svg>
 );
 });
-const CircleUserRound = React.memo(function CircleUserRound({ size = 18 }) {
+const CircleUserRound = React.memo(function ({ size = 18 }) {
 return (
 <svg width={size} height={size} viewBox="0 0 48 48">
 <path fill="#FFC107" d="M43.611,20.083H42V20H24v8h11.303c-1.649,4.657-6.08,8-11.303,8c-6.627,0-12-5.373-12-12s5.373-12,12-12c3.059,0,5.842,1.154,7.961,3.039l5.657-5.657C34.046,6.053,29.268,4,24,4C12.955,4,4,12.955,4,24s8.955,20,20,20s20-8.955,20-20C44,22.659,43.862,21.35,43.611,20.083z"/>
